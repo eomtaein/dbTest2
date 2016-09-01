26f9fa96be1578f137a62cedaeb248f6910a833f
Hoyeon-PC\Hoyeon <daera6@gmail.com>
2016-09-01 17:23:35 +0900

    타이틀바, 네비 , 옵션, 탭 레이아웃만

--- app/src/main/java/com/example/hoyeon/dbtest2/MainActivity.java
+++ app/src/main/java/com/example/hoyeon/dbtest2/MainActivity.java
@@ -1,13 +1,114 @@
 package com.example.hoyeon.dbtest2;
 
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.PersistableBundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private ListView drawerList;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.mainDrawerLayout);
+        drawerList = (ListView)findViewById(R.id.drawerlist);
+
+        toolbar = (Toolbar)findViewById(R.id.main_toolbar);
+        if(toolbar != null){
+            toolbar.setNavigationIcon(R.drawable.airtumblertoolbar_menu);
+            toolbar.setTitle("전화번호부태잉");
+            toolbar.setTitleTextColor(Color.WHITE);
+            setSupportActionBar(toolbar);
+        }
+
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
+        mDrawerLayout.setDrawerListener(drawerToggle);
+        String[] listValues = new String[]{ "등록", "수정", "삭제"};
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, listValues);
+        drawerList.setAdapter(listAdapter);
+
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.customtab);
+
+        tabLayout.addTab(tabLayout.newTab().setText("남자"));
+        tabLayout.addTab(tabLayout.newTab().setText("전체"));
+        tabLayout.addTab(tabLayout.newTab().setText("여자"));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/hoyeon/dbtest2/customTabbar.java
+++ app/src/main/java/com/example/hoyeon/dbtest2/customTabbar.java
@@ -1,10 +1,13 @@
 package com.example.hoyeon.dbtest2;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Hoyeon on 2016-09-01.
@@ -24,7 +27,19 @@ public class customTabbar extends TabLayout{
         super(context, attrs, defStyleAttr);
     }
 
-    
+    @Override
+    public void addTab( Tab tab) {
+        super.addTab(tab);
+
+        ViewGroup mainView = (ViewGroup)getChildAt(0);
+        ViewGroup tabView = (ViewGroup)mainView.getChildAt(tab.getPosition());
+
+        int tabChiledCount = tabView.getChildCount();
+        for(int i = 0; i <= tabChiledCount; i++){
+            View tabViewChild = tabView.getChildAt(i);
+        }
 
 
+
+    }
 }
