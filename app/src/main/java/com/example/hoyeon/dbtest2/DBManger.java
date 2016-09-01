1dd5c7db708aceca7d2c23cc596d21068d1cb404
Hoyeon-PC\Hoyeon <mt.sparrow@gmail.com>
2016-09-01 14:50:16 +0900

    시작단계

---
+++ app/src/main/java/com/example/hoyeon/dbtest2/DBManger.java
@@ -0,0 +1,29 @@
+package com.example.hoyeon.dbtest2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Hoyeon on 2016-09-01.
+ */
+public class DBManger extends SQLiteOpenHelper {
+
+
+    public DBManger(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL();
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
